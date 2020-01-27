862f9097e5ff373231fa1953f7ed5ac99068f710
John Christy <jchristy40@gmail.com>
2020-01-27 17:39:24 -0600

    add feedback for uploaded POST data (not working in response for some reason)

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,6 @@
-var exec = require("child_process").exec;
+var querystring = require("querystring");
 
-function start(response) {
+function start(response, postData) {
     console.log("Request handler 'start' was called.");
     var body = '<html>' +
         '<head>' +
@@ -21,11 +21,11 @@ function start(response) {
         response.end();
 }
 
-function upload(response) {
+function upload(response, postData) {
     console.log("Request handler 'upload' was called.");
-    
     response.writeHead(200, {"Content-type": "text/plain"});
-    response.write("Hello Uploader");
+    response.write("You've sent the text: "+ 
+    querystring.parse(postData).text);
     response.end();
 }
 
